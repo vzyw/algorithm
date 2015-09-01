20791f9ca80d955961024f3db3246d3694561f63
vzyw <vzyw@iCloud.com>
2015-09-01 19:54:24 +0800

    dfs_application

--- dfsApp.cpp
+++ dfsApp.cpp
@@ -35,7 +35,69 @@ public:
 	}
 };
 
+//检测是否是无环图
+class Cycle{
+private:
+	bool *marked;
+	bool cycle;
+	//v表示起点，n表示终点，
+	void dfs(Graph&g, int v, int n){
+		marked[v] = true;
+		for (std::list<AdjList>::iterator i = g.getAdj(v)->begin(); i != g.getAdj(v)->end(); i++){
+			//递归，把与起点的相邻的下一个点作为起点，起点作为终点。 如果找到了与当前起点相邻的下一个点被标记过了，说明之前经过这点，一定能构成环。
+			if (!marked[i->value])
+				dfs(g, i->value,v); 
+			else if (i->value != n) cycle = true;
+		}
 
+	}
+public:
+	Cycle(Graph&g){
+		hasCycle = false;
+		marked = new bool[g.getV()];
+		for (int n = 0; n < g.getV(); n++){
+			if (!marked[n]){
+				dfs(g, n, n);
+			}
+		}
+	}
+	bool hasCycle(){
+		return cycle;
+	}
+};
+
+//检测是否是二分图，用染色法，即从其中一个顶点开始，将跟它邻接的点染成与其不同的颜色，如果邻接的点有相同颜色的，则说明不是二分图，每次用bfs遍历即可。
+class bipartiteGraph{
+private:
+	bool *marked;
+	bool *color;//true 和false 示不同的两种颜色；
+	bool isBip;
+	void dfs(Graph&g, int node){
+		marked[node] = true;
+		for (std::list<AdjList>::iterator i = g.getAdj(node)->begin(); i != g.getAdj(node)->end(); i++){
+			if (!marked[i->value]){
+				color[i->value] = !color[node];
+				dfs(g, i->value);
+			}
+			else if (color[i->value] == color[node])isBip= false;  //已经判断过的点和当前点颜色一样说明不是二分图
+
+		}
+		
+	}
+public:
+	bipartiteGraph(Graph&g){
+		isBip= true;
+		color = new bool[g.getV()];
+		marked = new bool[g.getV()];
+		memset(marked, 0, g.getV()*sizeof(bool));
+		memset(color, 0, g.getV()*sizeof(bool));
+		dfs(g, 0);
+	}
+	bool isBipartite(){
+		return isBip;
+	}
+
+};
 int main(){
 	
 	std::ifstream in("c:\\t\\g.txt");
